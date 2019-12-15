ae6db9587d95ab8f608a7cf5506d2c4eabf93a9c
Voidmirror <voidmirror@users.noreply.github.com>
2019-12-15 19:32:11 +0400

    Kadane function

--- AlgoMains/maxSumSubmatrix.cpp
+++ AlgoMains/maxSumSubmatrix.cpp
@@ -5,6 +5,35 @@
 
 using namespace std;
 
+int kadaneAlgothythm(int* arr, int n) {
+	int sum = 0;
+	int negative = 0;
+	int maxSum = INT_MIN;
+
+	for (int i = 0; i < n; i++) {
+		sum += arr[i];
+		if (sum < 0) {
+			sum = 0;
+		}
+		else if (sum > maxSum) {
+			maxSum = sum;
+			negative++;
+		}
+	}
+
+	if (!negative) {
+		return maxSum;
+	}
+
+	maxSum = arr[0];
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > maxSum) {
+			maxSum = arr[i];
+		}
+	}
+	return maxSum;
+}
+
 int main() {
 
 }
